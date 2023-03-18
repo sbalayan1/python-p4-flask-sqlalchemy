a1f76a1d43074cf92afa877bd63569edea0a2ccc
sbalayan1 <balayans2014@yahoo.com>
2023-03-17 20:05:05 -0700

    update

--- server/app.py
+++ server/app.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy #Flask-SQLALchemy is a wrapper for SQLAlchemy
+from flask_migrate import Migrate #imports the Migrate class
 
-app = Flask(__name__)
+#flask migrate has identical commands to Alembic however instead of alembic revision for instance, we'd say flask db revision
+from models import db
+
+app = Flask(__name__) #creates an instance of the flask class with the module's name
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db' #the above sets our SQLALCHEMY_DATABASE_URI config variable to 'sqlite:///app.db'. Typicall you'd do this in alembic.ini.
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #without this line, we'll get a warning to update config variables in our alembic.ini. However we do this in app.py
+
+migrate = Migrate(app, db) #creates a new migrate instance by passing our Flask application instance and our SQLAlchemy instance
+db.init_app(app) #initializes a flask application for use with THIS databse(extension) instance. 
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,4 +1,4 @@
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy #instead of importing fields, we import the sqlAclhemy class and utilize that class's attributes to populate your fields
 from sqlalchemy import MetaData
 
 metadata = MetaData(naming_convention={
@@ -6,3 +6,24 @@ metadata = MetaData(naming_convention={
 })
 
 db = SQLAlchemy(metadata=metadata)
+class Owner(db.Model): #instead of using Base as the parent object, we use the db's Model class.
+    __tablename__ = 'owners'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    pets = db.relationship('Pet', backref='owner')
+
+    def __repr__(self):
+        return f'<Pet Owner {self.name}>'
+    
+class Pet(db.Model):
+    __tablename__ = 'pets'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    species = db.Column(db.String)
+
+    owner_id = db.Column(db.Integer, db.ForeignKey('owners.id'))
+
+    def __repr__(self):
+        return f'<Pet {self.name}, {self.species}>'
